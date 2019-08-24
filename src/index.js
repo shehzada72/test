b60f70bfb34b9d49b11c9e5fd2adb19d3280fbef
Shehzad Aslam <shehzada.pk786@gmail.com>
2019-08-24 18:00:06 +0500

    State Functional and class based component jsx
    Sat 24-08

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './pages/home';
+// import Home from './pages/home';
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+class App extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            institute: 'Fast Tech',
+            campus: 'Model town',
+        }
+    }
+
+    clickEvent = () => {
+        console.log('clicked!!!!!');
+    };
+
+
+    valueChanged = event => {
+        console.log(event.target.value);
+        this.setState({
+            institute: event.target.value
+        });
+    };
+
+    render() {
+        return <h1 onClick={this.clickEvent}>
+            <div>Hello {this.state.institute}</div>
+            <input value={this.state.institute} onChange={this.valueChanged}/>
+        </h1>
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
