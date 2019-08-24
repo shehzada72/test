6123a053e23144c91ab915042c587ae60d549c35
Shehzad Aslam <shehzada.pk786@gmail.com>
2019-08-24 17:00:08 +0500

    initial commit

---
+++ src/pages/home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from "../components/home/header";
+import Content from "../components/home/content";
+import Footer from "../components/home/footer";
+
+const Home = () => {
+    return (
+        <div>
+            <Header/>
+            <Content/>
+            <Footer/>
+        </div>
+    );
+};
+
+export default Home;
