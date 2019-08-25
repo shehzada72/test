89d3d06e78638303866ac4d2f563f3e10233a409
Shehzad Aslam <shehzada.pk786@gmail.com>
2019-08-25 17:16:16 +0500

    delete by filter method

--- src/index.js
+++ src/index.js
@@ -1,33 +1,74 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {exercises} from "./store";
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import InboxIcon from '@material-ui/icons/Inbox';
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+
 // import Home from './pages/home';
 
 class App extends React.Component {
-    constructor(){
+    constructor() {
         super();
         this.state = {
-            institute: 'Fast Tech',
-            campus: 'Model town',
+            exercises: []
         }
     }
 
-    clickEvent = () => {
-        console.log('clicked!!!!!');
+    componentDidMount() {
+        this.setState({exercises: exercises})
     };
 
 
-    valueChanged = event => {
-        console.log(event.target.value);
-        this.setState({
-            institute: event.target.value
-        });
+    handleDeleteExercise = id => {
+        let exercises = this.state.exercises.filter(ex => ex.id !== id);
+
+
+        this.setState({exercises});
     };
 
     render() {
-        return <h1 onClick={this.clickEvent}>
-            <div>Hello {this.state.institute}</div>
-            <input value={this.state.institute} onChange={this.valueChanged}/>
-        </h1>
+        console.log(this.state.exercises);
+
+        return (
+            <div>
+                <Grid item xs={12}>
+                    <Grid container justify="center">
+                            <Grid item>
+                                <Paper  >
+                                    <List component="nav" aria-label="main mailbox folders">
+                                        {this.state.exercises.map((exercise, index) => {
+                                            return (
+                                                <ListItem key={index} button>
+                                                    <ListItemIcon onClick={() => this.handleDeleteExercise(exercise.id)}>
+                                                        <InboxIcon />
+                                                    </ListItemIcon>
+                                                    <ListItemText primary={exercise.title} />
+                                                </ListItem>
+                                            )
+                                        })}
+
+                                    </List>
+                                </Paper>
+                            </Grid>
+
+                        <Grid item>
+                            <Paper  >
+                                {this.state.exercises.length}
+                            </Paper>
+                        </Grid>
+
+                    </Grid>
+
+                </Grid>
+                <Divider />
+            </div>
+        )
     }
 }
 
